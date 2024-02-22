ad8b0b3f99ce2c1aa9752b525089f47bd107ed59
Franco <fireburning353@gmail.com>
2024-02-22 09:58:41 +0630

    Update File

---
+++ LearnNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
